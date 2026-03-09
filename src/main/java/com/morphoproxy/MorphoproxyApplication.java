efc660b259257d31070a46feb135ecc8eb987ab4
Sanjeevi kumar <sanjeevisk1414@gmail.com>
2026-03-09 21:58:15 +0530

    Morpho-Proxy - Organized

--- src/main/java/com/morphoproxy/MorphoproxyApplication.java
+++ src/main/java/com/morphoproxy/MorphoproxyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MorphyproxyApplication {
+public class MorphoproxyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MorphyproxyApplication.class, args);
+		SpringApplication.run(MorphoproxyApplication.class, args);
 	}
 
 }
